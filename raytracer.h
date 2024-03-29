c0ec1320f9771b8d5ea8e0f9f95c798f511d26bc
Michael Tuntang <mike.tuntang547@gmail.com>
2024-03-29 01:41:00 -0700

    added random double utility function

--- raytracer.h
+++ raytracer.h
@@ -11,13 +11,23 @@ using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
-// Constants
+// CONSTANTS
 const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
-// Utility Functions
+// UTILITY FUNCTIONS
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+// Returns a random real in [0,1).
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// Returns a random real in [min,max).
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 #include "ray.h"
