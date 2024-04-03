f07c0d0158fb095b3b58d628c2656b1a4dc59086
Michael Tuntang <mike.tuntang547@gmail.com>
2024-04-02 17:53:51 -0700

    implemented gamma correction

--- color.h
+++ color.h
@@ -5,6 +5,16 @@
 
 #include <iostream>
 
+/*
+* Raises each color channel to the power of 1/2.
+* Gamma correction typically applied as one of the last steps before outputting the final image. 
+A common approach is to use a gamma value of 2.2, but using 2 instead for simplicity.
+*/
+inline double linear_to_gamma(double linear_component)
+{
+    return sqrt(linear_component);
+}
+
 void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
@@ -15,6 +25,11 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel) {
     r *= scale;
     g *= scale;
     b *= scale;
+
+    // Apply gamma correction
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Write the translated [0,255] value of each color component.
     static const interval intensity(0.000, 0.999);
