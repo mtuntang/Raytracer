838af404ee0a8719dfa98de013b9581169957fbf
Michael Tuntang <mike.tuntang547@gmail.com>
2024-04-25 01:35:12 -0700

    added camera positioning

--- main.cpp
+++ main.cpp
@@ -26,5 +26,11 @@ int main() {
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
+    cam.vfov = 25;
+    // X horizontal, Y vertical, Z (+) from scrren, (-) behind of screen to viewer 
+    cam.lookfrom = point3(-2, 2, 1);
+    cam.lookat = point3(0, 0, -1);
+    cam.vup = vec3(0, 1, 0);
+
     cam.render(world);
 }
